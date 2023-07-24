08d698678f63428e28fbd4fbe159c96ebf4cec90
Panyawat_KRONGKITICHU <pan.kkt@gmail.com>
2023-07-24 22:38:18 +0700

    tag fin.

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -2,18 +2,22 @@ import React from "react";
 import {View, Text, StyleSheet, Image} from "react-native"
 
 import yo from "../../assets/images/search.png";
+import {tag} from "../data/dev-data"
 
-const Activity = () =>{
+export default function Activity(){
     return (
         <View style={styles.container}>
-          {/* <Text style={{color:"white"}}>Ayo</Text> */}
-            <Image
-                style={styles.Apic}
-                source={yo}
-                
-                />
-            
-            {/* <Text style={styles.textColor}>gggggg</Text> */}
+          <Image
+              style={styles.pic}
+              source={yo}
+          />
+          {Array.from({length: tag.length}).map((_, index) => (
+            <View style={styles.tagCon}>
+              <Text key={index} style={styles.tagText}>
+                {tag[index]}
+              </Text>
+            </View>
+          ))}
         </View>
     );
   };
@@ -21,19 +25,27 @@ const Activity = () =>{
   const styles = StyleSheet.create({
     container: {
       backgroundColor: "black",
-      borderRadius: 30,
-      // padding: 150,
-      width: "100%"
+      borderRadius: 25,
+      width: "100%",
+      padding: 10,
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      rowGap: 10,
+      columnGap: 10,
     },
-    textColor: {
-      color: "#FFFFFF"
+    tagText: {
+      color: "black",
+      padding: 5,
     },
-    Apic: {
-        zIndex: 0,
-        width: '100%',
-        resizeMode: 'contain',
-
+    pic: {
+      width: '100%',
+      resizeMode: 'contain',
+      
+    },
+    tagCon: {
+      backgroundColor: "white",
+      alignSelf: 'flex-start',   
+      borderRadius: 40,   
+      
     }
   });
-
-  export default Activity;
