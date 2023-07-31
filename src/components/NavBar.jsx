6f13d9b2e0207d94264f54df99d326ac7923c6fc
Jeammm <athicha.phjkl@gmail.com>
2023-07-31 14:01:36 +0700

    fix hamburgur animation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,8 +15,10 @@ import filter_icon from "../../assets/images/filter.png";
 import search_icon from "../../assets/images/search.png";
 import plus_icon from "../../assets/images/plus.png";
 
+const ANIMATION_TIME = 200
+
 export default function NavBar() {
-  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+  const [hamburgerOpen, setHamburgerOpen] = useState(null);
   const { setCreatingNewMarker, setNewMarker, isLoggedIn, setIsLoggedIn } =
     useAppContext();
 
@@ -42,40 +44,51 @@ export default function NavBar() {
   };
 
   const rotateValueHolder = new Animated.Value(0);
-  const heightValueHolder = useState(new Animated.Value(0))[0];
+  const heightValueHolder = new Animated.Value(0);
 
   const RotateData = rotateValueHolder.interpolate({
     inputRange: [0, 1],
     outputRange: ["0deg", "90deg"],
   });
 
+  const HeightData = heightValueHolder.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 200],
+  });
 
   useEffect(() => {
-    rotateValueHolder.setValue(hamburgerOpen ? 0 : 1);
-    Animated.timing(rotateValueHolder, {
-      toValue: hamburgerOpen ? 1 : 0,
-      duration: 300,
-      useNativeDriver: true,
-    },
-    ).start();
-   if (hamburgerOpen) {
+    if (hamburgerOpen === null) { 
+      return
+    }
+    if (hamburgerOpen) {
+      Animated.timing(rotateValueHolder, {
+        toValue: 1,
+        duration: ANIMATION_TIME,
+        useNativeDriver: true,
+      }).start();
+
+      // heightValueHolder.setValue(hamburgerOpen ? 0 : 1);
       Animated.timing(heightValueHolder, {
-        toValue: 200,
-        duration: 300, 
+        toValue: 1,
+        duration: ANIMATION_TIME,
         useNativeDriver: false,
       }).start();
     } else {
+      rotateValueHolder.setValue(1);
+      Animated.timing(rotateValueHolder, {
+        toValue: 0,
+        duration: ANIMATION_TIME,
+        useNativeDriver: true,
+      }).start();
+      
+      heightValueHolder.setValue(1);
       Animated.timing(heightValueHolder, {
         toValue: 0,
-        duration: 150,
+        duration: ANIMATION_TIME,
         useNativeDriver: false,
       }).start();
     }
-   
-  },
-   [hamburgerOpen]);
-
-
+  }, [hamburgerOpen]);
 
   return (
     <View style={styles.container}>
@@ -91,22 +104,20 @@ export default function NavBar() {
             ]}
             source={hamburger_icon}
           />
-          <Animated.View
-            style={[styles.menus, { display: hamburgerOpen ? "flex" : "none", height: heightValueHolder,
-            
-           }]}
-          >
-            <Pressable onPress={() => alert("search")}>
-              <Image style={styles.menuItem} source={search_icon} />
-            </Pressable>
-            <Pressable onPress={() => alert("filer")}>
-              <Image style={styles.menuItem} source={filter_icon} />
-            </Pressable>
-            {isLoggedIn && <Pressable onPress={addButtonHandler}>
-              <Image style={styles.menuItem} source={plus_icon} />
-            </Pressable>}
-          </Animated.View>
         </Pressable>
+        <Animated.View style={[styles.menus, { height: HeightData }]}>
+          <Pressable onPress={() => alert("search")}>
+            <Image style={styles.menuItem} source={search_icon} />
+          </Pressable>
+          <Pressable onPress={() => alert("filer")}>
+            <Image style={styles.menuItem} source={filter_icon} />
+          </Pressable>
+          {isLoggedIn && (
+            <Pressable onPress={addButtonHandler}>
+              <Image style={styles.menuItem} source={plus_icon} />
+            </Pressable>
+          )}
+        </Animated.View>
         <Text style={styles.logo}>ZideQuest</Text>
       </View>
       {isLoggedIn ? (
@@ -151,20 +162,17 @@ const styles = StyleSheet.create({
   menus: {
     position: "absolute",
     backgroundColor: "#E86A33",
-    display: "flex",
     flexDirection: "column",
     justifyContent: "space-around",
-    paddingHorizontal: 5,
     top: 60,
-    padding: 10,
-    gap: 10,
     borderRadius: 100,
+    overflow: "hidden",
   },
   menuItem: {
     width: 30,
     height: 30,
-    padding: 10,
-
+    padding: 20,
+    margin: 5,
   },
   logo: {
     color: "#E86A33",
